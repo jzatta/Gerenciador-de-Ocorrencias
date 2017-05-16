132bd7b85e4e1809cca45551895de60f02cba680
Juliano Kasmirski Zatta <julianokzatta@gmail.com>
2017-05-16 10:48:09 -0300

    Adding project

---
+++ src/br/inf/ocorrecia/teste/testeFuncionario.java
@@ -0,0 +1,13 @@
+package br.inf.ocorrecia.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testeFuncionario {
+
+	@Test
+	public void testeCadastroFuncionario() throws Exception {
+		assertTrue(true);
+	}
+}
